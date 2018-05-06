a3777990f7b34137c163055220fc2c7fcb478d7f
Joseph Park <joseph.thomas.park@gmail.com>
2018-05-06 11:01:16 +1000

    Add links to nav-bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   resources :categories
   devise_for :users
-  resources :listings
+  resources :listings do
+    resources :categories
+  end
+
   root to: 'listings#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
