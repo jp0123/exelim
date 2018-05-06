e59c7383afec6c123fd5cd1e12553092d3076a4c
Joseph Park <joseph.thomas.park@gmail.com>
2018-05-06 16:23:06 +1000

    Association

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,4 @@
 class Category < ApplicationRecord
-    has_many :listings
 
     mount_uploader :image, ImageUploader
 end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,5 @@
 class Listing < ApplicationRecord
     belongs_to :user
-    belongs_to :category
 
     mount_uploader :image, ImageUploader
 end
