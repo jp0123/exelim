25bb02b2bb8d1c3d5ecefc97d52636a3b9b0d050
Joseph Park <joseph.thomas.park@gmail.com>
2018-05-06 15:41:21 +1000

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   resources :categories
   devise_for :users
-  resources :listings do
-    resources :categories
-  end
+  resources :listings
 
   root to: 'listings#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
