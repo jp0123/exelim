823a7c4cf81ddf1feaae390a3c401dccac47aa3c
Joseph Park <joseph.thomas.park@gmail.com>
2018-05-07 08:15:29 +1000

    Add pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   resources :categories
   devise_for :users
   resources :listings
+  
+  get 'pages/home', to: 'pages#home'
+  get 'pages/about', to: 'pages#about'
+  get 'pages/contact', to: 'pages#contact'
+  get 'pages/dashboard', to: 'pages#dashboard'
 
   root to: 'listings#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
