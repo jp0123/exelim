fda9112e4451e5c52d5453a3c634a357b196f092
Joseph Park <joseph.thomas.park@gmail.com>
2018-05-08 11:52:06 +1000

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get 'pages/about', to: 'pages#about'
   get 'pages/contact', to: 'pages#contact'
   get 'pages/dashboard', to: 'pages#dashboard'
+  get 'users', to: 'pages#dashboard'
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
