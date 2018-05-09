47806947c5b4e05cc451e90508a6cd949b8d6966
Joseph Park <joseph.thomas.park@gmail.com>
2018-05-09 15:53:25 +1000

    Add Listing Model validationså

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,10 @@
 class Listing < ApplicationRecord
+    validates :title, presence: true
+    validates :description, presence: true
+    validates :price, presence: true
+    validates :image, format: {with: /\.(png|jpg)\Z/i}
+    
     belongs_to :user
-
+    
     mount_uploader :image, ImageUploader
 end
